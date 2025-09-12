e83c8245cb35c78e58a3e9fab6d608b69bb219bc
asmasultana07 <ab.asma1084@gmail.com>
2025-09-12 23:37:37 +0600

    almost done

--- js/script.js
+++ js/script.js
@@ -47,8 +47,19 @@ getElement("service-box").addEventListener('click', function(e){
     }
 });
 
+// heart count work
+getElement("service-box").addEventListener('click', function(e){
+    if(e.target.className.includes("click-copy")){
+        // console.log("second click");
+        const targetBtn = e.target;
+        const serviceNumber = targetBtn.parentNode.parentNode.children[1].children[2].innerText;
+        const totalCopy = getElement("total-copy").innerText;
+        const currentCopy = Number(totalCopy) + 1;
+        getElement("total-copy").innerText = currentCopy;
+    }
+    
 
-
+})
 // // heart count
 // document.getElementById("click-heart").addEventListener('click', function(e){
 //     e.preventDefault()
@@ -123,42 +134,3 @@ getElement("service-box").addEventListener('click', function(e){
 //     addHeartCount.innerText = countValue+1;
 // })
 
-// // call button function
-
-// document.getElementById('click-call')
-// .addEventListener('click', function(){
-//     alert('📞 Calling জাতীয় জরুরি সেবা 999');
-// })
-
-// document.getElementById('click-call2')
-// .addEventListener('click', function(){
-//     alert('📞 Calling পুলিশ সেবা 999');
-// })
-// document.getElementById('click-call3')
-// .addEventListener('click', function(){
-//     alert('📞 Calling ফায়ার সার্ভিস 999');
-// })
-// document.getElementById('click-call4')
-// .addEventListener('click', function(){
-//     alert('📞 Calling অ্যাম্বুলেন্স 1994-999999');
-// })
-// document.getElementById('click-call5')
-// .addEventListener('click', function(){
-//     alert('📞 Calling নারী ও শিশু সহায়তা 109');
-// })
-// document.getElementById('click-call6')
-// .addEventListener('click', function(){
-//     alert('📞 Calling দুদক 106');
-// })
-// document.getElementById('click-call7')
-// .addEventListener('click', function(){
-//     alert('📞 Calling বিদ্যুৎ বিভ্রাট 16216');
-// })
-// document.getElementById('click-call8')
-// .addEventListener('click', function(){
-//     alert('📞 Calling ব্র্যাক 16445');
-// })
-// document.getElementById('click-call9')
-// .addEventListener('click', function(){
-//     alert('📞 Calling বাংলাদেশ রেলওয়ে 163');
-// })
